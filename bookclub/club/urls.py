3ed6f41b62bf3321aab3ccb5948e474df35addfb
elishamjco <elishacote@cmail.carleton.ca>
2023-11-07 17:36:15 -0500

    added delete book

--- bookclub/club/urls.py
+++ bookclub/club/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, BookDetail, AddBookView, UpdateBookView
+from .views import HomeView, BookDetail, AddBookView, UpdateBookView, DeleteBookView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('book/<int:pk>', BookDetail.as_view(), name="book-details"),
     path('add_book/', AddBookView.as_view(), name="add-book" ),
     path('book/edit/<int:pk>', UpdateBookView.as_view(), name="edit-book" ),
+    path('book/<int:pk>/remove', DeleteBookView.as_view(), name="delete-book" ),
 ]
--- bookclub/club/views.py
+++ bookclub/club/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Book
+from django.urls import reverse_lazy
 # Create your views here.
 
 class HomeView(ListView):
@@ -20,3 +21,8 @@ class UpdateBookView(UpdateView):
     model = Book
     template_name = 'update_book.html'
     fields = '__all__'
+
+class DeleteBookView(DeleteView):
+    model = Book
+    template_name = 'delete_book.html'
+    success_url = reverse_lazy('home')
