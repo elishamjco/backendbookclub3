e7b4ce20c66a1a43a283a37923cc1de1ad33d3ab
elishamjco <elishacote@cmail.carleton.ca>
2023-11-07 19:11:50 -0500

    added genre model

--- bookclub/club/models.py
+++ bookclub/club/models.py
@@ -3,12 +3,20 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 # Create your models here.
 
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
 class Book(models.Model):
     book = models.CharField(max_length=50)
     description = models.TextField()
     author = models.CharField(max_length=50)
     release_date = models.DateField()
-    genre = models.CharField(max_length=50)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.book
