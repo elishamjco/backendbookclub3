9a7d95ee2eebfe49c70a75df6b291bfff77fee81
elishamjco <elishacote@cmail.carleton.ca>
2023-11-07 19:21:06 -0500

    added more models

--- bookclub/club/admin.py
+++ bookclub/club/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Book, Genre
+from .models import Book, Genre, Message, Club, Reviews
 
 admin.site.register(Book)
 admin.site.register(Genre)
+admin.site.register(Message)
+admin.site.register(Club)
+admin.site.register(Reviews)
--- bookclub/club/models.py
+++ bookclub/club/models.py
@@ -23,3 +23,36 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('home')
+
+class Reviews(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    text = models.TextField(blank=True)
+    rating = models.IntegerField(default=0)
+    time = models.DateTimeField()
+
+    def __str__(self):
+        return self.book + self.user
+    def get_absolute_url(self):
+        return reverse('home')
+
+class Club(models.Model):
+    name = models.CharField(max_length=200)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    host = models.ForeignKey(User,on_delete=models.CASCADE)
+    description= models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name + self.book
+    def get_absolute_url(self):
+        return reverse('home')
+
+class Message(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    message = models.TextField()
+    time = models.DateTimeField()
+
+    def __str__(self):
+        return self.user
+    def get_absolute_url(self):
+        return reverse('home')
