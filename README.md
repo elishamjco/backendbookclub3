611df66fa6402a8bd5a81ad4f45936608ad2cfa9
elishamjco <elishacote@cmail.carleton.ca>
2023-12-05 23:35:35 -0500

    re-final commit

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Git hub repository: https://github.com/elishamjco/backendbookclub3
 To run project, first ensure that the requirements are installed. 
 <br>Run server in manage.py to view backend at localhost:8000
 <br>View Api at localhost:800/api/*insert page here* 
-to run front end run npm star and view localhost:3000, localhost:3000/register is also viewable however posting does not work
+to run front end run npm start and view localhost:3000
 
 
-Users can make accounts, book clubs, books, edit and delete books as well as edit and delete clubs. Reviews can now be made, are automatically assigned to a user and do appear!
-Unfortunatley this is far from a well working web application; however, this is all i was able to complete.
+In the backend Users can make accounts, book clubs, books, edit and delete books as well as edit and delete clubs. Reviews can now be made, are automatically assigned to a user and do appear!
+In the frontend you can view a list of books and clubs that will update if the backend is changed. When you hover over the books a tooltip showing the author should appear.
+There also is a button for a dark mode.
+Unfortunately this is far from a well working web application; however, this is all i was able to complete.
--- bookclub/frontend/src/App.js
+++ bookclub/frontend/src/App.js
@@ -1,33 +1,65 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import AddBook from './components/AddBook';
-import AddClub from './components/AddClub';
-import ViewClubs from './components/ViewClubs';
-import ViewBooks from './components/ViewBooks';
-import Login from './components/Login';
-import Register from './components/Register';
-import BookDetails from './components/BookDetails';
-import ClubDetails from './components/ClubDetails';
-import PostReview from './components/PostReview';
+import React, { useState, useEffect } from 'react';
+import { Tooltip } from 'react-tooltip';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './HomePage.css';
 
-const App = () => {
+const HomePage = () => {
+    const [books, setBooks] = useState([]);
+    const [clubs, setClubs] = useState([]);
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/api/books/')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error fetching books');
+                }
+                return response.json();
+            })
+            .then(data => setBooks(data))
+            .catch(error => console.error('Error fetching books:', error));
+
+        fetch('http://localhost:8000/api/clubs/')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error fetching clubs');
+                }
+                return response.json();
+            })
+            .then(data => setClubs(data))
+            .catch(error => console.error('Error fetching clubs:', error));
+    }, []);
+
+    const toggleDarkMode = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
     return (
-        <Router>
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/add-book" component={AddBook} />
-                <Route path="/add-club" component={AddClub} />
-                <Route path="/clubs" component={ViewClubs} />
-                <Route path="/books" component={ViewBooks} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-                <Route path="/bookdetails/:bookId" component={BookDetails} />
-                <Route path="/clubdetails/:clubId" component={ClubDetails} />
-                <Route path="/postreview" component={PostReview} />
-            </Switch>
-        </Router>
+        <div className={`home-page ${isDarkMode ? 'dark-mode' : ''}`}>
+            <center>
+                <h1>Books</h1>
+                <ul>
+                    {books.map(book => (
+                        <li key={book.id} data-tooltip-id={`my-tooltip-${book.id}`}>
+                            Name: {book.book} Description: {book.description}
+                            <Tooltip id={`my-tooltip-${book.id}`} place="top" type="info" effect="solid">
+                                Author: {book.author}
+                            </Tooltip>
+                        </li>
+                    ))}
+                </ul>
+
+                <h1>Clubs</h1>
+                <ul>
+                    {clubs.map(club => (
+                        <li key={club.id}>Name: {club.name} Description: {club.description}</li>
+                    ))}
+                </ul>
+
+                <button onClick={toggleDarkMode}>{isDarkMode ? 'Light Mode' : 'Dark Mode'}</button>
+            </center>
+        </div>
     );
 };
 
-export default App;
+export default HomePage;
